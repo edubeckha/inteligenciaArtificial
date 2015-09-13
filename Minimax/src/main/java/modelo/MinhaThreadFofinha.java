a44c4b7080d99f3b737555f6b62d10d79514ef5d
Rodrigo Mello <pmello.rodrigor@gmail.com>
2015-09-13 17:34:08 -0300

    threads para verificar sequencia

---
+++ Minimax/src/main/java/modelo/MinhaThreadFofinha.java
@@ -0,0 +1,131 @@
+package modelo;
+
+public class MinhaThreadFofinha extends Thread{
+
+    public int ordem, melhorSequenciaHumano, melhorSequenciaIA;
+    int contadorSequenciaIA = 0, contadorSequenciaHumano = 0, pivo = 0;
+
+    public TipoThread tipoThread;
+    public int[][] matrizElementos;
+
+    public MinhaThreadFofinha(int nOrdem, TipoThread th, int[][] matriz) {
+        ordem = nOrdem;
+        tipoThread = th;
+        matrizElementos = matriz;
+    }
+
+    @Override
+    public void run() {
+        switch (tipoThread) {
+            case LINHA:
+                iteraLinha();
+                break;
+            case COLUNA:
+                iteraColuna();
+                break;
+            case DIAGONALPRINCIPAL:
+                iteraDiagonalPrincipal();
+                break;
+            case DIAGONALSECUNDARIA:
+                iteraDiagonalSecundaria();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void iteraLinha() {
+        for (int i = 0; i < ordem; i++) {
+            contadorSequenciaHumano = 0;
+            contadorSequenciaIA = 0;
+            for (int j = 0; j < ordem; j++) {
+                verificaMatriz(i, j, matrizElementos);
+            }
+        }
+        System.out.println("humano Linha" + melhorSequenciaHumano);
+        System.out.println("IA Linha" + melhorSequenciaIA);
+
+    }
+
+    private void iteraColuna() {
+        for (int i = 0; i < ordem; i++) {
+            contadorSequenciaHumano = 0;
+            contadorSequenciaIA = 0;
+            for (int j = 0; j < ordem; j++) {
+                verificaMatriz(j, i, matrizElementos);
+            }
+        }
+
+        System.out.println("humano Coluna" + melhorSequenciaHumano);
+        System.out.println("IA Coluna" + melhorSequenciaIA);
+    }
+
+    private void iteraDiagonalPrincipal() {
+        for (int i = 0; i < ordem; i++) {
+            verificaMatriz(i, i, matrizElementos);
+        }
+
+        System.out.println("humano Diagonal Principal " + melhorSequenciaHumano);
+        System.out.println("IA Diagonal Principal" + melhorSequenciaIA);
+    }
+
+    
+    //TODO: fazer depois
+    private void iteraDiagonalSecundaria() {
+        for (int i = 0; i < ordem; i++) {
+            contadorSequenciaHumano = 0;
+            contadorSequenciaIA = 0;
+            for (int j = 0; j < ordem; j++) {
+
+            }
+        }
+
+    }
+
+    public void verificaMatriz(int i, int j, int[][] tabuleiro) {
+
+        if (tabuleiro[i][j] == 0) { //acabou a sequencia e a casa proxima eh vazia
+            pivo = 0;
+            contadorSequenciaHumano = 0;
+            contadorSequenciaIA = 0;
+            return;
+        }
+        //a jogada mudou
+        if (tabuleiro[i][j] != pivo && pivo != 0) { //alguem me barrou - comeca outra sequencia
+            if (pivo == 1) { //caso anterior, para atualizar o valor de maxContador
+                if (contadorSequenciaHumano > melhorSequenciaHumano) {
+                    melhorSequenciaHumano = contadorSequenciaHumano;
+                }
+                contadorSequenciaHumano = 0;
+                pivo = 2;
+                contadorSequenciaIA++;
+            } else {
+                if (contadorSequenciaIA > melhorSequenciaIA) {
+                    melhorSequenciaIA = contadorSequenciaIA;
+                }
+                contadorSequenciaIA = 0;
+                pivo = 1;
+                contadorSequenciaHumano++;
+            }
+            return;
+        }
+
+        //continua com a mesma sequencia do jogador
+        if (tabuleiro[i][j] == 1) { //eh do humano
+            contadorSequenciaHumano++;
+            pivo = 1;
+            if (contadorSequenciaHumano > melhorSequenciaHumano) {
+                melhorSequenciaHumano = contadorSequenciaHumano;
+            }
+            return;
+        }
+        if (tabuleiro[i][j] == 2) {
+            contadorSequenciaIA++;
+            pivo = 2;
+            if (contadorSequenciaIA > melhorSequenciaIA) {
+                melhorSequenciaIA = contadorSequenciaIA;
+            }
+        }
+    }
+
+}
--- Minimax/src/main/java/modelo/Sequencia.java
+++ Minimax/src/main/java/modelo/Sequencia.java
@@ -10,131 +10,62 @@ public class Sequencia {
 
     public TipoSequencia tipoSequencia;
     public ArrayList<Identificador> idsJogadas;
-    public ArrayList<TipoSequencia> sequenciasHumano, sequenciasIA;
+    public ArrayList<Integer> sequenciasMaximasHumano, sequenciasMaximasIA;
+    int melhorSequenciaHumano, melhorSequenciaIA;
 
     public Sequencia() {
+        sequenciasMaximasHumano = new ArrayList<>();
+        sequenciasMaximasIA = new ArrayList<>();
         idsJogadas = new ArrayList<>();
-        sequenciasHumano = new ArrayList<>();
-        sequenciasIA = new ArrayList<>();
-        //tipoSequencia = verificaCasas(); 
     }
 
     //passar por todo o array de identificadores para criar uma determinada sequencia (tiposequencia)
-    private TipoSequencia verificaCasas(int linhas, int colunas, int[][] tabuleiro) {
-        int contadorSequenciaIA = 0, contadorSequenciaHumano = 0, maxContadorHumano = 0, maxContadorIA = 0;
-        int pivo = 0;
+    synchronized private TipoSequencia verificaCasas(int ordem, int[][] tabuleiro) {
 
-        /**
-         * Itera sobre as colunas da matriz, recebendo uma linha
-         *
-         * O pivo começa em zero e assim que receber um valor != 0, começa a
-         * contar a sequencia. A partir do momento que a casa muda de valor, o
-         * contador sabera qual o tamanho da sequencia
-         *
-         */
-        for (int i = 0; i < linhas; i++) {
-            for (int j = 0; j < colunas; j++) {
-                if (tabuleiro[i][j] == 0) { //acabou a sequencia e a casa proxima eh vazia
-                    pivo = 0;
-                    contadorSequenciaHumano = 0;
-                    contadorSequenciaIA = 0;
-                    continue;
-                }
-
-                //a jogada mudou
-                if (tabuleiro[i][j] != pivo && pivo != 0) { //alguem me barrou - comeca outra sequencia
-                    if (pivo == 1) { //caso anterior, para atualizar o valor de maxContador
-                        if (contadorSequenciaHumano > maxContadorHumano) {
-                            maxContadorHumano = contadorSequenciaHumano;
-                            
-                        }
-                        contadorSequenciaHumano = 0;
-                        pivo = 2;
-                        contadorSequenciaIA++;
-                    } else {
-                        if (contadorSequenciaIA > maxContadorIA) {
-                            maxContadorIA = contadorSequenciaIA;
-                        }
-                        contadorSequenciaIA = 0;
-                        pivo = 1;
-                        contadorSequenciaHumano++;
-                    }
-                    continue;
-                }
-
-                //continua com a mesma sequencia do jogador
-                if (tabuleiro[i][j] == 1) { //eh do humano
-                    contadorSequenciaHumano++;
-                    pivo = 1;
-                    if (contadorSequenciaHumano > maxContadorHumano) {
-                        maxContadorHumano = contadorSequenciaHumano;
-                    }
-                    continue;
-                }
-                if (tabuleiro[i][j] == 2) {
-                    contadorSequenciaIA++;
-                    pivo = 2;
-                    if (contadorSequenciaIA > maxContadorIA) {
-                        maxContadorIA = contadorSequenciaIA;
-
-                    }
-                }
-
+        ArrayList<MinhaThreadFofinha> thread = new ArrayList<>();
+        thread.add(new MinhaThreadFofinha(ordem, TipoThread.LINHA, tabuleiro));
+        thread.add(new MinhaThreadFofinha(ordem, TipoThread.COLUNA, tabuleiro));
+        thread.add(new MinhaThreadFofinha(ordem, TipoThread.DIAGONALPRINCIPAL, tabuleiro));
+        //   thread.add(new MinhaThreadFofinha(ordem, TipoThread.DIAGONALSECUNDARIA, tabuleiro));
+        int countThreads = 0;
+        try {
+            for (MinhaThreadFofinha thread1 : thread) {
+                thread1.run();
+                thread1.join();
             }
+        } catch (Exception ex) {
+
         }
-        System.out.println("humano " + maxContadorHumano + " contador " + contadorSequenciaHumano);
-        System.out.println("ia " + maxContadorIA + " ia " + contadorSequenciaIA);
+
         return null;
-    }
-    
-    public void verificaMatriz(){
-        
     }
 
     public static void main(String[] args) {
-        int[][] matriz = new int[1][11];
+        int[][] matriz = new int[4][4];
 
         matriz[0][0] = 1;
         matriz[0][1] = 1;
-        matriz[0][2] = 2;
-        matriz[0][3] = 2;
-        matriz[0][4] = 1;
-        matriz[0][5] = 1;
-        matriz[0][6] = 1;
-        matriz[0][7] = 1;
-        matriz[0][8] = 1;
-        matriz[0][9] = 0;
-        matriz[0][10] = 1;
+        matriz[0][2] = 1;
+        matriz[0][3] = 1;
 
+        matriz[1][0] = 0;
+        matriz[1][1] = 2;
+        matriz[1][2] = 2;
+        matriz[1][3] = 1;
 
-        /* matriz[1][0] = 0;
-         matriz[1][1] = 0;
-         matriz[1][2] = 0;
-         matriz[1][3] = 2;
-         matriz[1][4] = 2;
-       
-       
-         matriz[2][0] = ;
-         matriz[2][1] = 1;
-         matriz[2][2] = 1;
-         matriz[2][3] = 1;
-         matriz[2][4] = 1;
-       
-       
-         matriz[3][0] = 1;
-         matriz[3][1] = 1;
-         matriz[3][2] = 1;
-         matriz[3][3] = 1;
-         matriz[3][4] = 1;
-       
-       
-         matriz[4][0] = 1;
-         matriz[4][1] = 1;
-         matriz[4][2] = 1;
-         matriz[4][3] = 1;
-         matriz[4][4] = 1;  */
+        matriz[2][0] = 0;
+        matriz[2][1] = 0;
+        matriz[2][2] = 2;
+        matriz[2][3] = 1;
+
+        matriz[3][0] = 0;
+        matriz[3][1] = 0;
+        matriz[3][2] = 2;
+        matriz[3][3] = 0;
+
         Sequencia sequencia = new Sequencia();
-        sequencia.verificaCasas(1, 11, matriz);
+
+        sequencia.verificaCasas(4, matriz);
 
     }
 
