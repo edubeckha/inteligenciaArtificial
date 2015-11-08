c94a382ac693aaab3a250e147f3894ef95b23ac0
Rodrigo Mello <pmello.rodrigor@gmail.com>
2015-11-08 15:35:17 -0200

    leitura dos valores invalida

--- LogicaFuzzy/LineFollowerSimulator_source/src/LineFollowerSimulator/RegulatorClient.java
+++ LogicaFuzzy/LineFollowerSimulator_source/src/LineFollowerSimulator/RegulatorClient.java
@@ -23,117 +23,125 @@ import net.sourceforge.jFuzzyLogic.FIS;
  */
 public class RegulatorClient implements Runnable {
 
-  private volatile double p = 0.42187;
-  private volatile double i = 0.09375;
-  private volatile double d = 2.36718;
+    private volatile double p = 0.42187;
+    private volatile double i = 0.09375;
+    private volatile double d = 2.36718;
 
-  private Socket socket;
-  private PrintWriter out;
-  private BufferedReader in;
-  
-  private final String host;
-  private final int port;
-  
-  public RegulatorClient(String host, int port) {
-    this.host = host;
-    this.port = port;
-  }
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
 
-  public void run() {
+    private final String host;
+    private final int port;
+
+    public RegulatorClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void run() {
 // ********************Versão original*********************************
 /*
-    listenSocket(this.host, this.port);
-	while (socket.isConnected()) {
-      try {
-        String line = in.readLine();
-        String[] params = line.split(" ");
-        double position = Double.parseDouble(params[0]);
-        double sumLinePositions = Double.parseDouble(params[1]);
-        double previousLinePosition = Double.parseDouble(params[2]);
-        double drive = p * position + 
-                i * sumLinePositions + d * (position - previousLinePosition);
-        out.println(drive);
-      } catch (IOException ex) {
-        Logger.getLogger(RegulatorClient.class.getName()).log(Level.SEVERE, null, ex);
-        break;
-      }
-    }*/
+         listenSocket(this.host, this.port);
+         while (socket.isConnected()) {
+         try {
+         String line = in.readLine();
+         String[] params = line.split(" ");
+         double position = Double.parseDouble(params[0]);
+         double sumLinePositions = Double.parseDouble(params[1]);
+         double previousLinePosition = Double.parseDouble(params[2]);
+         double drive = p * position + 
+         i * sumLinePositions + d * (position - previousLinePosition);
+         out.println(drive);
+         } catch (IOException ex) {
+         Logger.getLogger(RegulatorClient.class.getName()).log(Level.SEVERE, null, ex);
+         break;
+         }
+         }*/
 // ***********************************************************************
-    
 
-    listenSocket(this.host, this.port);
-	while (socket.isConnected()) {
-      try {        
-        FIS fis = FIS.load("robot.fcl", true);
-        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-        String fromServer;
- 
-        double sensorEsquerda, sensorDireita;
-        // requisicao da posicao do carrinho verificar se é isso com o carrinho        
-        while ((fromServer = in.readLine()) != null) {
-        	StringTokenizer st = new StringTokenizer(fromServer);
-                System.out.println(st.nextToken());
-        	/*sensorEsquerda = Double.valueOf(st.nextToken());
-                
-        	sensorDireita = Double.valueOf(st.nextToken());
+        listenSocket(this.host, this.port);
+        while (socket.isConnected()) {
+            try {
+                FIS fis = FIS.load("robot.fcl", true);
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+                String fromServer;
 
-        	System.out.println("x: " + sensorEsquerda + " y: " + sensorDireita);*/
-        	
-                //seta os valores para alterar a posicao do robo.
-		/*fis.setVariable("sensor_left_value", 0.35);
-                fis.setVariable("sensor_right_value", 0.35);
-                
-		fis.evaluate();
+                double sensorEsquerda = 0;
+                double sensorDireita = 0;
+                // requisicao da posicao do carrinho verificar se é isso com o carrinho        
+                while ((fromServer = in.readLine()) != null) {
+                    StringTokenizer st = new StringTokenizer(fromServer);
+                    if (st.nextToken().equalsIgnoreCase("infinity")) {
+                        sensorEsquerda = 2;
+                    }
+                    
+                    st = new StringTokenizer(in.readLine());
+                    if (st.nextToken().equalsIgnoreCase("-infinity")) {
+                        sensorDireita = -2;
+                    } else {
 
-		double forcaDireita = fis.getVariable("motor_force_left").getLatestDefuzzifiedValue();
-                double forcaEsquerda = fis.getVariable("motor_force_right").getLatestDefuzzifiedValue();
-		System.out.println("Força esquerda lida: " + forcaEsquerda);
-                System.out.println("Força direita lida: " + forcaDireita);
-                            */
-        	// envio do comando ao motor
-        	out.println(0.2);
-                out.println(0.2);
-        	
+                //verificar casos opostos de infinity                 
+                        sensorEsquerda = Double.valueOf(st.nextToken());
+
+                        sensorDireita = Double.valueOf(st.nextToken());
+                    }
+                    System.out.println("esq: " + sensorEsquerda + " dir: " + sensorDireita);
+
+                    //seta os valores para alterar a posicao do robo.
+                    fis.setVariable("left", 0.0);
+                    fis.setVariable("right", 0.0);
+
+                    fis.evaluate();
+
+                    double forcaDireita = fis.getVariable("motor_force_left").getLatestDefuzzifiedValue();
+                    double forcaEsquerda = fis.getVariable("motor_force_right").getLatestDefuzzifiedValue();
+                    System.out.println("Força esquerda lida: " + forcaEsquerda);
+                    System.out.println("Força direita lida: " + forcaDireita);
+
+                    // envio do comando ao motor
+                    out.println(0.2);
+                    out.println(0.2);
+
                 //requisicao da posicao do carrinho        	
-        	
+                }
+
+                out.close();
+                in.close();
+                stdIn.close();
+
+                double drive = 0; // computar usando fazzy
+
+                //out.println(drive);
+            } catch (IOException ex) {
+                Logger.getLogger(RegulatorClient.class.getName()).log(Level.SEVERE, null, ex);
+                break;
+            }
         }
- 
-        out.close();
-        in.close();
-        stdIn.close();
-        
-        
-        double drive = 0; // computar usando fazzy
-        
-        //out.println(drive);
-      } catch (IOException ex) {
-        Logger.getLogger(RegulatorClient.class.getName()).log(Level.SEVERE, null, ex);
-        break;
-      }
     }
-  }
 
-  public void listenSocket(String host, int port) {
-    try {
-      socket = new Socket(host, port);
-      out = new PrintWriter(socket.getOutputStream(), true);
-      in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      System.out.println("Connected on " + host + ":" + port);
-    } catch (UnknownHostException e) {
-      System.out.println("Unknown host: " + host);
-      System.exit(1);
-    } catch (IOException e) {
-      System.out.println("No I/O");
-      System.exit(1);
+    public void listenSocket(String host, int port) {
+        try {
+            socket = new Socket(host, port);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println("Connected on " + host + ":" + port);
+        } catch (UnknownHostException e) {
+            System.out.println("Unknown host: " + host);
+            System.exit(1);
+        } catch (IOException e) {
+            System.out.println("No I/O");
+            System.exit(1);
+        }
     }
-  }
 
-  void finish() {
-    try {
-      if (socket != null) 
-        socket.close();
-    } catch (IOException ex) {
-      Logger.getLogger(RegulatorClient.class.getName()).log(Level.SEVERE, null, ex);
+    void finish() {
+        try {
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(RegulatorClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-  }
 }
